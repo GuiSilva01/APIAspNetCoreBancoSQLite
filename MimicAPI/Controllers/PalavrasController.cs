e36acea15d830e41d813998fe00716e31e5b53bb
Guilherme da Silva <guilherme.dasilvanascimento@gmail.com>
2020-07-08 16:09:36 -0300

    Metodos PalavrasController

--- MimicAPI/Controllers/PalavrasController.cs
+++ MimicAPI/Controllers/PalavrasController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MimicAPI.Database;
+using MimicAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,42 @@ namespace MimicAPI.Controllers
 {
     public class PalavrasController : ControllerBase
     {
+        private readonly MimicContext _banco;
+        public PalavrasController(MimicContext banco)
+        {
+            _banco = banco;
+        }
+
+        public ActionResult ObterPalavras()
+        {
+            return new JsonResult( _banco.Palavras);
+        }
+
+        public ActionResult Obter(int id)
+        {
+            return Ok(_banco.Palavras.Find(id));
+        }
+
+        public ActionResult Cadastrar(Palavra palavra)
+        {
+            _banco.Palavras.Add(palavra);
+
+            return Ok();
+        }
+
+        public ActionResult Atualizar (int id, Palavra palavra)
+        {
+            _banco.Palavras.Update(palavra);
+
+            return Ok();
+        }
+
+        public ActionResult Deletar(int id)
+        {
+            _banco.Palavras.Remove(_banco.Palavras.Find(id));
+
+            return Ok();
+        }
+
     }
 }
