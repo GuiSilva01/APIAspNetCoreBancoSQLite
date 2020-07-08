b57863d36ef56da8376fcafc039992a54b7bf049
Guilherme da Silva <guilherme.dasilvanascimento@gmail.com>
2020-07-08 18:10:45 -0300

    Ajuste e teste das requisições

--- MimicAPI/Controllers/PalavrasController.cs
+++ MimicAPI/Controllers/PalavrasController.cs
@@ -38,9 +38,10 @@ namespace MimicAPI.Controllers
         // Nesse caso estamos enviando para o servidor atras do metodo POST (id, nome, ativo, pontuacao, criacao)
         [Route("")]
         [HttpPost]
-        public ActionResult Cadastrar(Palavra palavra)
+        public ActionResult Cadastrar([FromBody]Palavra palavra)
         {
             _banco.Palavras.Add(palavra);
+            _banco.SaveChanges();
 
             return Ok();
         }
@@ -48,20 +49,24 @@ namespace MimicAPI.Controllers
         // PUT (id, nome, ativo, pontuacao, criacao)
         [Route("{id}")]
         [HttpPut]
-        public ActionResult Atualizar (int id, Palavra palavra)
+        public ActionResult Atualizar (int id, [FromBody]Palavra palavra)
         {
             palavra.Id = id;
             _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
 
             return Ok();
         }
 
         // DELETE (id, nome, ativo, pontuacao, criacao)
-        [Route("{id")] 
+        [Route("{id}")] 
         [HttpDelete]
         public ActionResult Deletar(int id)
         {
-            _banco.Palavras.Remove(_banco.Palavras.Find(id));
+            var palavra = _banco.Palavras.Find(id);
+            palavra.Ativo = false;
+            _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
 
             return Ok();
         }
